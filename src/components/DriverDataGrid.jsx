c4d358afc5cd1fa52d65b55cbaa0f642b23c32c8
markprubin <mark.p.rubin@gmail.com>
2023-06-07 11:57:47 -0700

    updates driverDataGrid

--- src/components/DriverDataGrid.jsx
+++ src/components/DriverDataGrid.jsx
@@ -3,21 +3,39 @@ import axios from "axios";
 import React from "react";
 import { DataGrid } from "@mui/x-data-grid";
 
-const rows = [
-  { id: 1, col1: "Hello", col2: "World" },
-  { id: 2, col1: "DataGridPro", col2: "is Awesome" },
-  { id: 3, col1: "MUI", col2: "is Amazing" },
-];
-
-const columns = [
-  { field: "col1", headerName: "Driver", width: 150 },
-  { field: "col2", headerName: "Points", width: 150 },
-];
-
 export function DriverDataGrid() {
+  const [driverStandings, setDriverStandings] = useState([]);
+
+  const handleDriverStandings = () => {
+    console.log("driverStandings");
+    axios.get("http://ergast.com/api/f1/current/driverStandings.json").then((response) => {
+      console.log(response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings);
+      setDriverStandings(response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings);
+    });
+  };
+
+  useEffect(handleDriverStandings, []);
+
+  const rows = driverStandings.map((driver) => ({
+    id: driver.Driver.driverId,
+    col1: `${driver.Driver.givenName} ${driver.Driver.familyName}`,
+    col2: driver.points,
+  }));
+
+  const columns = [
+    { field: "col1", headerName: "Driver", width: 170 },
+    { field: "col2", headerName: "Points", width: 40 },
+  ];
+
   return (
     <div>
-      <DataGrid rows={rows} columns={columns} />
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        sx={{ overflowY: "scroll", height: "450px" }}
+        hideFooter
+        disableSelectionOnClick
+      />
     </div>
   );
 }
